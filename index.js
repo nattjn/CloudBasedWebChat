79a2d1befccd6afe122a574f7f64fd9e01b824e4
pencilpram <pencilpram@gmail.com>
2022-10-25 18:29:59 +0700

    updated

--- index.js
+++ index.js
@@ -29,3 +29,59 @@ const botName = "ChatCord Bot";
     subClient = pubClient.duplicate();
     io.adapter(createAdapter(pubClient, subClient));
 })();
+
+// Run when client connects
+io.on("connection", (socket) => {
+    console.log(io.of("/").adapter);
+    socket.on("joinRoom", ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room);
+
+        // Welcome current user
+        socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
+
+        // Broadcast when a user connects
+        socket.broadcast
+            .to(user.room)
+            .emit(
+                "message",
+                formatMessage(botName, `${user.username} has joined the chat`)
+            );
+
+        // Send users and room info
+        io.to(user.room).emit("roomUsers", {
+            room: user.room,
+            users: getRoomUsers(user.room),
+        });
+    });
+
+    // Listen for chatMessage
+    socket.on("chatMessage", (msg) => {
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit("message", formatMessage(user.username, msg));
+    });
+
+    // Runs when client disconnects
+    socket.on("disconnect", () => {
+        const user = userLeave(socket.id);
+
+        if (user) {
+            io.to(user.room).emit(
+                "message",
+                formatMessage(botName, `${user.username} has left the chat`)
+            );
+
+            // Send users and room info
+            io.to(user.room).emit("roomUsers", {
+                room: user.room,
+                users: getRoomUsers(user.room),
+            });
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
