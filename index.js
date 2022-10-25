aede2716869395fb6fcd612df72ba576a8312a33
pencilpram <pencilpram@gmail.com>
2022-10-25 18:28:45 +0700

    updated

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ const {
     userLeave,
     getRoomUsers,
 } = require("./utils/users");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+const botName = "ChatCord Bot";
+
+(async () => {
+    pubClient = createClient({ url: "redis://127.0.0.1:6379" });
+    await pubClient.connect();
+    subClient = pubClient.duplicate();
+    io.adapter(createAdapter(pubClient, subClient));
+})();
